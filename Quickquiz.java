a040b61424da6600702c6fadc7b9dc75fbac4cea
Devendra Parmar <99470456+Devendraparmar2002@users.noreply.github.com>
2022-07-22 20:30:23 +0530

    Add files via upload

---
+++ Quickquiz.java
@@ -0,0 +1,15 @@
+/*
+-> Quick quiz program Of the Chapter-1
+-> Developed By : Devendra Parmar
+-> Topic : Add Three Number
+*/
+public class Quickquiz{
+    public static void main(String [] args){
+        System.out.print("Sum Of Three Number :");
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 10;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
