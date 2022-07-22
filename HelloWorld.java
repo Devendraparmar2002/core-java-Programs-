052e0e7fc3800a77a6de85a1df9617c7b62f17be
Devendra Parmar <99470456+Devendraparmar2002@users.noreply.github.com>
2022-07-22 20:29:21 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+Created By : Devendra Parmar
+*/
+
+//This is The Main Class of the Program
+public class HelloWorld{
+    //This is The Main Method And Entry point of the Program
+    public static void main(String [] aergs){
+        //This is The Print Statement of the Program
+        System.out.println("Hello World");
+    }
+}
